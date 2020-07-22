f09df6fbaa776f1b4fd88cadc37de0a6d9ca2d44
chandra-hasa <65844280+chandra-hasa@users.noreply.github.com>
2020-07-22 18:35:40 +0530

    Updates

---
+++ precache-manifest.ff98524db6f9fd1296ec952157df1c6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec7572ba1d82725743669bec19d195c",
+    "url": "/react_todos/index.html"
+  },
+  {
+    "revision": "e052ddf8e3fa084a5046",
+    "url": "/react_todos/static/js/2.fdd52fe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_todos/static/js/2.fdd52fe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d79d1d346f85cf9f76d",
+    "url": "/react_todos/static/js/main.0d43a1c0.chunk.js"
+  },
+  {
+    "revision": "1998793f459dd001eec4",
+    "url": "/react_todos/static/js/runtime-main.10af1ab3.js"
+  }
+]);
